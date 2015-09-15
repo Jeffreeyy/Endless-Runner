02309dc76402f7f45bbce04ecd846749e854bb83
Jeffreeyy <17844@ma-web.nl>
2015-09-15 11:53:55 +0200

    Added moving chunks and mana bar

--- Assets/Scripts/ChunkManager.cs
+++ Assets/Scripts/ChunkManager.cs
@@ -4,71 +4,58 @@ using System.Collections.Generic;
 
 public class ChunkManager : MonoBehaviour
 {
-    [Header("Chunk prefabs")]
-    public List<GameObject> m_chunks = new List<GameObject>();
-
-    //screen width in game unit
-    private float m_screenWidthGameUnits;
+	public int moveSpeed = 5;
+    public List<GameObject> chunks = new List<GameObject>();
+    private float _screenWidthGameUnits;
 	private bool added = false;
 
-	private List<GameObject> m_chunkClones = new List<GameObject>();
+	private List<GameObject> _chunkClones = new List<GameObject>();
 
     void Awake()
     {
-        this.m_screenWidthGameUnits = this.getHalfScreenWidth();
+        this._screenWidthGameUnits = this.getHalfScreenWidth();
     }
 
     void Start()
     {
 		for (int i = 0; i < 3; i++)
 		{
-			m_chunkClones.Add(getRandomChunk(Vector3.zero));
+			_chunkClones.Add(getRandomChunk(Vector3.zero));
 		}
-		for (int j = 0; j < m_chunkClones.Count; j++)
+		for (int j = 0; j < _chunkClones.Count; j++)
 		{
-			m_chunkClones[j].transform.position = new Vector3(j * getHalfScreenWidth()-getChunkWidth(m_chunkClones[j]),0);
+			_chunkClones[j].transform.position = new Vector3(j * getHalfScreenWidth()-getChunkWidth(_chunkClones[j]),0);
 		}
 
-		m_chunkClones[0].transform.position = new Vector3 (0 - m_screenWidthGameUnits, 0);
+		_chunkClones[0].transform.position = new Vector3 (0 - _screenWidthGameUnits, 0);
 
-		Vector3 eersteChunkPos = m_chunkClones[0].transform.position;
-		for (int k = 0; k < m_chunkClones.Count; k++)
+		Vector3 eersteChunkPos = _chunkClones[0].transform.position;
+		for (int k = 0; k < _chunkClones.Count; k++)
 		{
-			m_chunkClones[k].transform.position = new Vector3(eersteChunkPos.x+getChunkWidth(m_chunkClones[k])*k,0);
+			_chunkClones[k].transform.position = new Vector3(eersteChunkPos.x+getChunkWidth(_chunkClones[k])*k,0);
 		}
-        //maak alle chunks aan
-        //sorteer de nieuwe chunk zo dat ze het scherm volledig vullen
     }
 
     void Update()
     {
-        //check of alle chunks nog binne scherm zijn
-        //delete de chunks die buiten het scherm zijn
-        //beweeg alle chunks
 		if (added) {
-			m_chunkClones.Add(getRandomChunk(Vector3.zero));
+			_chunkClones.Add(getRandomChunk(Vector3.zero));
 			added = false;
 		}
-		sortChunks (m_chunkClones);
-		for (int i = 0;i < m_chunkClones.Count; i++)
+		sortChunks (_chunkClones);
+		for (int i = 0;i < _chunkClones.Count; i++)
 		{
-			moveChunk(m_chunkClones[i],1);
-			if(checkBoundsChunk(m_chunkClones[i]) == true)
+			moveChunk(_chunkClones[i],moveSpeed);
+			if(checkBoundsChunk(_chunkClones[i]) == true)
 			{
-				Destroy(m_chunkClones[i]);
-				m_chunkClones.RemoveAt(i);
+				Destroy(_chunkClones[i]);
+				_chunkClones.RemoveAt(i);
 				added = true;
-
-				//spawnChunk(m_chunkClones[i], Vector3.zero);
 			}
 		}
 
     }
 
-    /// <summary>
-    /// Sorteer de chunk achter elkaar
-    /// </summary>
-    /// <param name="_chunks">List van chunks die gesorteerd moeten worden</param>
     private void sortChunks(List<GameObject> _chunks)
     {
         if (_chunks.Count<1)
@@ -76,24 +63,16 @@ public class ChunkManager : MonoBehaviour
             Debug.Log("Error sort chunk! list heeft geen elementen");
             return;
         }
-        //var l_offset = m_screenWidthGameUnits;
-        //get first chunk position
         var l_firstChunkV3 = _chunks[0].transform.position;
-        //sort chunks
         for (int i = 0; i < _chunks.Count; i++)
         {
             _chunks[i].transform.position = new Vector3(l_firstChunkV3.x + (getChunkWidth(_chunks[i]) * i), 0);
         }
     }
 
-    /// <summary>
-    /// Check of de chunk aan de linker kant uit het scherm is
-    /// </summary>
-    /// <param name="_chunk">chunk die we gaan checken</param>
-    /// <returns>True = uit scherm, False = binnen scherm</returns>
     private bool checkBoundsChunk(GameObject _chunk)
     {
-        if (_chunk.transform.position.x < 0 - (m_screenWidthGameUnits + getChunkWidth(_chunk) / 2))
+        if (_chunk.transform.position.x < 0 - (_screenWidthGameUnits + getChunkWidth(_chunk) / 2))
         {
             return true;
         }
@@ -103,54 +82,28 @@ public class ChunkManager : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// Beweeg een chunk
-    /// </summary>
-    /// <param name="_chunk">Chunk dat bewongen moet worden</param>
-    /// <param name="_speed">Snelheid van bewegen</param>
     private void moveChunk(GameObject _chunk, float _speed)
     {
         _chunk.transform.position -= new Vector3(_speed * Time.deltaTime, 0);
     }
 
-    /// <summary>
-    /// Haal een random chunk op
-    /// </summary>
-    /// <param name="_position">positie van game object</param>
-    /// <returns>chunk clone gameobject</returns>
     private GameObject getRandomChunk(Vector3 _position)
     {
-        return spawnChunk(m_chunks[Random.Range(0, m_chunks.Count)], _position);
+        return spawnChunk(chunks[Random.Range(0, chunks.Count)], _position);
     }
-
-    /// <summary>
-    /// Spawn een chunk
-    /// </summary>
-    /// <param name="_chunk">chunk game object</param>
-    /// <param name="_position">position van game object</param>
-    /// <returns>chunk clone</returns>
+	
     private GameObject spawnChunk(GameObject _chunk, Vector3 _position)
     {
         return (GameObject)Instantiate(_chunk, _position, Quaternion.identity);
     }
 
-    /// <summary>
-    /// Haal de breete op van de chunk
-    /// </summary>
-    /// <param name="_chunk">chunk game object</param>
-    /// <returns>breete in float</returns>
     private float getChunkWidth(GameObject _chunk)
     {
         return _chunk.GetComponent<BoxCollider2D>().size.x;
     }
 
-    /// <summary>
-    /// Haal de helft van de schermbreete op in game units
-    /// </summary>
-    /// <returns>game with in game units</returns>
     private float getHalfScreenWidth()
     {
-        //orthoWidth = orthographicSize / screenHeight * screenWidth;
         return (Camera.main.orthographicSize / Camera.main.pixelHeight * Camera.main.pixelWidth);
     }
 }
